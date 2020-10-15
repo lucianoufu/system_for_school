7cdb4711fe3dbb2b0afe890c067b4ab2257b7f91
Luciano <soaresluciano85@gmail.com>
2020-10-15 10:21:42 -0300

    Correction of the function insert from the adm user

--- main.py
+++ main.py
@@ -53,9 +53,9 @@ def fn_db_table_creation():
         ,discipline        VARCHAR(30)
         ,note              NUMERIC(2,1) NOT NULL
         ,date_application  TIMESTAMP    NOT NULL
-        ,class             NUMERIC(2,1)
+        ,class             VARCHAR(10)
         ,FOREIGN KEY (id_student) REFERENCES tb_student(id)
-        ,FOREIGN KEY (id_teacher) REFERENCES tb_teacher(id_teacher)
+        ,FOREIGN KEY (id_teacher) REFERENCES tb_teacher(id)
     );'''
 
     create_query_test2 = '''CREATE TABLE tb_test2
@@ -65,6 +65,7 @@ def fn_db_table_creation():
         ,discipline       VARCHAR(30)
         ,note             NUMERIC(2,1) NOT NULL
         ,date_application TIMESTAMP    NOT NULL
+        ,class             VARCHAR(10)
         ,FOREIGN KEY (id_student) REFERENCES tb_student(id)
         ,FOREIGN KEY (id_teacher) REFERENCES tb_teacher(id_teacher)
     );'''
@@ -76,6 +77,7 @@ def fn_db_table_creation():
         ,discipline       VARCHAR(30)
         ,note             NUMERIC(2,1) NOT NULL
         ,date_application TIMESTAMP    NOT NULL
+        ,class            VARCHAR(10)
         ,FOREIGN KEY (id_student) REFERENCES tb_student(id)
         ,FOREIGN KEY (id_teacher) REFERENCES tb_teacher(id_teacher)
     );'''
@@ -87,8 +89,8 @@ def fn_db_table_creation():
         logging.error('Table tb_student already exists or the query has an error')
 
     try:
-        create_stmt = ibm_db.exec_immediate(conn, create_query_parent)
-        logging.debug('Table tb_parent created.')
+        create_stmt = ibm_db.exec_immediate(conn, create_query_teacher)
+        logging.debug('Table tb_teacher created.')
     except:
         logging.error('Table tb_parent alreadt exists or the query has an error.')
 
@@ -114,51 +116,35 @@ def fn_student_loging():
     print()
     while True:
         f_name = input('Digite o nome do aluno: ').title()
-        senha = input('Digite a sua senha: ')
-        sql = 'select * from tb_student where f_name = ' + f_name + ' and keyword = ' + senha + ';'
+        l_name = input('Digite o seu sobrenome: ').title()
+        password = input('Digite a sua senha: ')
+        sql = "SELECT * FROM tb_student WHERE f_name = '{}' and l_name = '{}' and password = '{}'".format(f_name, l_name, password)
         stmt = ibm_db.exec_immediate(conn, sql)
-        dictonary = ibm_db.fetch_tuple(stmt)
+        tuple_sql = ibm_db.fetch_tuple(stmt)
         if dictonary == ():
             print('Usuário ou senha incorreto.')
         else:
-            ###########################################################################
-            #
-            #>>>>>>>>>>>>>>>>>>>>>> Continue here <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-            #
-            ###########################################################################
-            print('Usuário e senha estão corretos :).')
-            #fn_student_show_note()
+            fn_shown_notes(tuple_sql[0])
 
-#def fn_student_show_note(name):
-#    sql = '''
-#    SELECT p1.note
-#          ,p2.note
-#          ,p3.note
-#    FROM tb_note1
-#    INEER JOIN
-#    (
-#        tb_note2 as p2
-#        INNER JOIN tb_note3 as p3
-#        ON p3.id = p2.id
-#    )
-#    ON p2.id = p1.id'''
-#    stmt = ibm_db.exec_immediate(conn, sql)
-#    dict = ibm_db.fetch_assoc(stmt)
-#    print(dict)
-
-
-def show_student():
-    sql = 'SELECT * FROM tb_student'
+def fn_show_notes(id_db):
+    sql = '''SELECT s.f_name
+    ,t.f_name
+    ,p1.note
+    FROM 
+    tb_student as s
+    INNER JOIN
+    (
+        tb_teacher as t INNER JOIN
+        tb_note1 as p1
+        ON t.id = p1.id_teacher
+    )
+    ON p1.id_student = s.id
+    '''
     stmt = ibm_db.exec_immediate(conn, sql)
-    dictonary = ibm_db.fetch_assoc(stmt)
-    while (dictonary) != False:
-        students['ID'].append(dictonary['ID'])
-        students['F_NAME'].append(dictonary['F_NAME'])
-        students['L_NAME'].append(dictonary['L_NAME'])
-        dictonary = ibm_db.fetch_assoc(stmt)
-    student_df = pd.DataFrame(students)
-    print(student_df.loc[:, ['F_NAME', 'L_NAME']])
-    fn_student_loging()
+    tuple_sql = ibm_db.fetch_tuple(stmt)
+    print('Aluno: {} \nProfessor: {} \nNota P1: {}'.format(tuple_sql[0], tuple_sql[1], tuple_sql[2]))
+            
+
 
 def fn_adm_options():
     opcao = input('\n\nEscolha uma opção: \n1 - Inserir aluno. \n2 - Deletar aluno. \n3 - Atualizar dados de um aluno. \n4 - Inserir professor. \n5 - Deletar professor \n6 - Atualizar professor. \nDigite: ')
@@ -185,24 +171,45 @@ def fn_insert(name):
     id_db = input('Digite o id do {}: '.format(name_pt))
     f_name = input('Digite o nome do {}: '.format(name_pt))
     l_name = input('Digite o sobrenome do {}: '.format(name_pt))
-    clas_s = input('Digite a classe do {}: '.format(name_pt))
-    password = input('Digite a senha do {}: '.format(name_pt))
-    sql = '''INSERT INTO ''' + 'tb_' + name + ''' 
-    (id
-    ,f_name
-    ,l_name
-    ,class
-    ,password
-    )
-    VALUES
-    ('{}', '{}', '{}', '{}', '{}');'''.format(id_db, f_name, l_name, clas_s, password)
+    if name == 'student':
+        clas_s = input('Digite a classe do {}: '.format(name_pt))
+        password = input('Digite a senha do {}: '.format(name_pt))
+        sql = '''INSERT INTO ''' + 'tb_' + name + ''' 
+        (
+        id
+        ,f_name
+        ,l_name
+        ,class
+        ,password
+        )
+        VALUES
+        ({}, '{}', '{}', '{}', '{}');'''.format(id_db, f_name, l_name, clas_s, password)
+    else:
+        sql = '''INSERT INTO tb_{}
+        (
+        id
+        ,f_name
+        ,l_name
+        )
+        VALUES
+        ({}, '{}', '{}');'''.format(name, id_db, f_name, l_name)
     
+    
+    stmt = ibm_db.exec_immediate(conn, sql)
     try:
-        stmt = ibm_db.exec_immediate(conn, sql)
-        logging.debug('Value ({}, {}, {}, {},{}); insert into tb_{}.'.format(id_db, f_name, l_name, clas_s, password, name))
+        if name == 'student':
+            logging.debug('Value ({}, {}, {}, {},{}); insert into tb_{}.'.format(id_db, f_name, l_name, clas_s, password, name))
+            print('{} inserido com sucesso.\n\n'.format(name_pt))
+        else:
+            print('{} inserido com sucesso.\n\n'.format(name_pt))
+            logging.debug('Value ({}, {}, {}); insert into tb_{}.'.format(id_db, f_name, l_name, name))
     except:
-        print('Valor não inserido.')
-        logging.error('Value ({}, {}, {}, {},{}); not insert into tb_{}.'.format(id_db, f_name, l_name, clas_s, password, name))
+        print('{} não inserido.\n'.format(name_pt))
+        if name == 'student':
+            logging.error('Value ({}, {}, {}, {},{}); not insert into tb_{}.'.format(id_db, f_name, l_name, clas_s, password, name))
+        else:
+            print('{} não inserido.\n'.format(name_pt))
+            logging.error('Value ({}, {}, {}); not insert into tb_{}.'.format(id_db, f_name, l_name, name))
 
 def fn_delete(name):
     #Teste para comentário
@@ -307,10 +314,10 @@ fn_db_table_creation()
 
 students = {'ID': [], 'F_NAME': [], 'L_NAME': []}
 while True:
-    opcao = input('Escolha uma opção: \n1 - Acessar como name/responsavel. \n2 - Acessar como professor. \n3 - Sair \nDigite: ')
+    opcao = input('Escolha uma opção: \n1 - Acessar como aluno/responsavel. \n2 - Acessar como professor. \n3 - Sair \nDigite: ')
 
     if opcao == '1':
-        show_student()
+        fn_student_loging()
     elif opcao == '2':
         print('Prof escolhido.')
     elif opcao == '3':
